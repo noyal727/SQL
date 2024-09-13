2cc572074b1b4436e593a2390f43e88a98be40c7
Jonnalagadda Noyal <61744610+noyal727@users.noreply.github.com>
2024-09-13 11:41:04 -0700

    Time: 1218 ms (40.10%) | Memory: 0B (100.00%) - LeetSync

--- Leetcode_SQL/1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ Leetcode_SQL/1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select unique_id, name from Employees LEFT JOIN EmployeeUNI USING(id) 
+select unique_id, name from EmployeeUNI Right JOIN Employees USING(id) 
